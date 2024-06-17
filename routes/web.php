b236e3809b75803fc610a51f275e6fb78ee43eec
mohammad <mohammadkh12221@gmail.com>
2024-06-17 16:32:15 +0330

    delete video page

--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,10 @@ use Inertia\Inertia;
 
 Route::get('/', function () {
     return Inertia::render('Welcome');
-});
-
+})->name("home");
+Route::get('/delete-video', function () {
+    return Inertia::render('DeleteVideo');
+})->name("deleteVideo");
 Route::get("/videos/{id}" , [VideoController::class , "show"])->name("video.show");
 
 require __DIR__ . '/auth.php';
